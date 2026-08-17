9ec1d9c0aa78fc22ff2147d4679eaec83a8bc535
anees_ahmad <anees_ahmad@vecros.com>
2026-08-17 05:13:18 +0000

    changed database url

--- app/settings.py
+++ app/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str = "postgresql://postgres:password@localhost:5432/payments"
+    database_url: str = "postgresql://neondb_owner:npg_mMeSAb2np3tg@ep-proud-hat-ayhs82g6.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require"
 
     class ConfigDict:
         env_file = ".env"
